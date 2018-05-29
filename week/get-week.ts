0019036b6a74ddb94843e0512aeeb74dccc7b300
dmitry <dmitryporotov@gmail.com>
2018-05-29 19:11:03 +0300

    Added code to find week number

---
+++ week/get-week.ts
@@ -0,0 +1,68 @@
+/**
+ * @licence
+ *
+ * Copyright (c) 2014-2015 [Charlike Make Reagent](http://j.mp/1stW47C), contributors
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @param {string | Date} date
+ * @returns {number}
+ */
+
+export function currentWeekNumber(date: string | Date): number {
+  let instance;
+
+  if (typeof date === 'string' && date.length) {
+    instance = new Date(date);
+  } else if (date instanceof Date) {
+    instance = date;
+  } else {
+    instance = new Date();
+  }
+
+  // Create a copy of this date object
+  const target = new Date(instance.valueOf());
+
+  // ISO week date weeks start on monday
+  // so correct the day number
+  const dayNr = (instance.getDay() + 6) % 7;
+
+  // ISO 8601 states that week 1 is the week
+  // with the first thursday of that year.
+  // Set the target date to the thursday in the target week
+  target.setDate(target.getDate() - dayNr + 3);
+
+  // Store the millisecond value of the target date
+  const firstThursday = target.valueOf();
+
+  // Set the target to the first thursday of the year
+  // First set the target to january first
+  target.setMonth(0, 1);
+  // Not a thursday? Correct the date to the next thursday
+  if (target.getDay() !== 4) {
+    target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+  }
+
+  // The weeknumber is the number of weeks between the
+  // first thursday of the year and the thursday in the target week
+  return 1 + Math.ceil((firstThursday - (target as any)) / 604800000);
+}
